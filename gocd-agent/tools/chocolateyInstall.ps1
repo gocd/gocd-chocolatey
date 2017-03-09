c32d3ee891d4898c848a8bfb505de2033bbfc795
Varsha Varadarajan <varshasvaradarajan@gmail.com>
2017-03-09 22:10:41 +0530

    Use chocolatey-core.extension for installer arguments.

--- gocd-agent/tools/chocolateyInstall.ps1
+++ gocd-agent/tools/chocolateyInstall.ps1
@@ -1,9 +1,17 @@
+$packageParameters = Get-PackageParameters
+
 $packageName = 'gocd-agent'
 $installerType = 'exe'
 $version = $env:version
 $revision = $env:revision
 $url = "https://download.gocd.io/binaries/${version}-${revision}/win/go-agent-${version}-${revision}-jre-32bit-setup.exe"
 $url64 = "https://download.gocd.io/binaries/${version}-${revision}/win/go-agent-${version}-${revision}-jre-64bit-setup.exe"
-$silentArgs = '/S'
+$args = '/S'
+
+if ($pp["SERVERURL"] -ne $null -or $pp["SERVERURL"] -ne '') { $args += " /SERVERURL=$packageParameters['SERVERURL']" }
+if ($pp["GO_AGENT_JAVA_HOME"] -ne $null -or $pp["GO_AGENT_JAVA_HOME"] -ne '') { $args += " /GO_AGENT_JAVA_HOME=$packageParameters['GO_AGENT_JAVA_HOME']" }
+if ($pp["PATH_TO_AGENT_DIRECTORY"] -ne $null -or $pp["PATH_TO_AGENT_DIRECTORY"] -ne '') { $args += " /D=$packageParameters['PATH_TO_AGENT_DIRECTORY']" }
+
 $validExitCodes = @(0)
 
+Install-ChocolateyPackage "$packageName" "$installerType" "$args" "$url" "$url64"  -validExitCodes $validExitCodes
--- gocd-server/tools/chocolateyInstall.ps1
+++ gocd-server/tools/chocolateyInstall.ps1
@@ -1,9 +1,16 @@
+$packageParameters = Get-PackageParameters
+
 $packageName = 'gocd-server'
 $installerType = 'exe'
 $version = $env:version
 $revision = $env:revision
 $url = "https://download.gocd.io/binaries/${version}-${revision}/win/go-server-${version}-${revision}-jre-32bit-setup.exe"
 $url64 = "https://download.gocd.io/binaries/${version}-${revision}/win/go-server-${version}-${revision}-jre-64bit-setup.exe"
-$silentArgs = '/S'
+$args = '/S'
+
+if ($pp["GO_SERVER_JAVA_HOME"] -ne $null -or $pp["GO_SERVER_JAVA_HOME"] -ne '') { $args += " /GO_SERVER_JAVA_HOME=$packageParameters['GO_SERVER_JAVA_HOME']" }
+if ($pp["PATH_TO_SERVER_DIRECTORY"] -ne $null -or $pp["PATH_TO_SERVER_DIRECTORY"] -ne '') { $args += " /D=$packageParameters['PATH_TO_SERVER_DIRECTORY']" }
+
 $validExitCodes = @(0)
 
+Install-ChocolateyPackage "$packageName" "$installerType" "$args" "$url" "$url64"  -validExitCodes $validExitCodes
