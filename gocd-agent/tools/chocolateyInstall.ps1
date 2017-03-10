43ae0ef25805c929349c9c8a0d35e9a5482a6e4f
Varsha Varadarajan <varshasvaradarajan@gmail.com>
2017-03-10 21:11:38 +0530

    Change the install scrip to remove package parameters. Use install arguments instead.

--- gocd-agent/tools/chocolateyInstall.ps1
+++ gocd-agent/tools/chocolateyInstall.ps1
@@ -1,17 +1,10 @@
-$packageParameters = Get-PackageParameters
-
 $packageName = 'gocdagent'
 $installerType = 'exe'
 $version = $env:version
 $revision = $env:revision
 $url = "https://download.gocd.io/binaries/${version}-${revision}/win/go-agent-${version}-${revision}-jre-32bit-setup.exe"
 $url64 = "https://download.gocd.io/binaries/${version}-${revision}/win/go-agent-${version}-${revision}-jre-64bit-setup.exe"
-$args = '/S'
-
-if ($packageParameters["SERVERURL"] -ne $null -or $packageParameters["SERVERURL"] -ne '') { $args += " /SERVERURL=$packageParameters['SERVERURL']" }
-if ($packageParameters["GO_AGENT_JAVA_HOME"] -ne $null -or $packageParameters["GO_AGENT_JAVA_HOME"] -ne '') { $args += " /GO_AGENT_JAVA_HOME=$packageParameters['GO_AGENT_JAVA_HOME']" }
-if ($packageParameters["PATH_TO_AGENT_DIRECTORY"] -ne $null -or $packageParameters["PATH_TO_AGENT_DIRECTORY"] -ne '') { $args += " /D=$packageParameters['PATH_TO_AGENT_DIRECTORY']" }
-
+$silentArgs = '/S'
 $validExitCodes = @(0)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$args" "$url" "$url64"  -validExitCodes $validExitCodes
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
--- gocd-server/tools/chocolateyInstall.ps1
+++ gocd-server/tools/chocolateyInstall.ps1
@@ -1,16 +1,10 @@
-$packageParameters = Get-PackageParameters
-
 $packageName = 'gocdserver'
 $installerType = 'exe'
 $version = $env:version
 $revision = $env:revision
 $url = "https://download.gocd.io/binaries/${version}-${revision}/win/go-server-${version}-${revision}-jre-32bit-setup.exe"
 $url64 = "https://download.gocd.io/binaries/${version}-${revision}/win/go-server-${version}-${revision}-jre-64bit-setup.exe"
-$args = '/S'
-
-if ($packageParameters["GO_SERVER_JAVA_HOME"] -ne $null -or $packageParameters["GO_SERVER_JAVA_HOME"] -ne '') { $args += " /GO_SERVER_JAVA_HOME=$packageParameters['GO_SERVER_JAVA_HOME']" }
-if ($packageParameters["PATH_TO_SERVER_DIRECTORY"] -ne $null -or $packageParameters["PATH_TO_SERVER_DIRECTORY"] -ne '') { $args += " /D=$packageParameters['PATH_TO_SERVER_DIRECTORY']" }
-
+$silentArgs = '/S'
 $validExitCodes = @(0)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$args" "$url" "$url64"  -validExitCodes $validExitCodes
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
