dbba93f51088e89f2dac64edacb4b87eadbbb3dc
Alex <alexbestul@users.noreply.github.com>
2017-05-19 11:00:21 -0500

    Improve path handling

--- createPackage.ps1
+++ createPackage.ps1
@@ -11,11 +11,13 @@ param(
 $path = "gocd-" + $type
 $fullVersion = $version + "-" + $revision
 
-cd $path
+Push-Location $path
+
 $checksumFileContent = (New-Object System.Net.WebClient).DownloadString("https://download.gocd.io/binaries/$fullVersion/win/go-$type-$fullVersion-jre-32bit-setup.exe.sha256sum")
 $checksum32 = $checksumFileContent.Split(" ")[0]
 $checksumFileContent = (New-Object System.Net.WebClient).DownloadString("https://download.gocd.io/binaries/$fullVersion/win/go-$type-$fullVersion-jre-64bit-setup.exe.sha256sum")
 $checksum64 = $checksumFileContent.Split(" ")[0]
 (Get-Content tools\chocolateyInstall.ps1.template) -replace '{{fullVersion}}', $fullVersion -replace '{{checksum-32bit}}', $checksum32 -replace '{{checksum-64bit}}', $checksum64 | out-file tools\chocolateyInstall.ps1;
 choco pack --version=$version
-cd ..
+
+Pop-Location
