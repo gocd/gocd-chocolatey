00d7b1094f1d23871bcd0ade61fadad0deded011
Alex <alexbestul@users.noreply.github.com>
2017-05-19 11:03:02 -0500

    Simplify checksum fetching code

--- createPackage.ps1
+++ createPackage.ps1
@@ -13,10 +13,9 @@ $fullVersion = $version + "-" + $revision
 
 Push-Location $path
 
-$checksumFileContent = (New-Object System.Net.WebClient).DownloadString("https://download.gocd.io/binaries/$fullVersion/win/go-$type-$fullVersion-jre-32bit-setup.exe.sha256sum")
-$checksum32 = $checksumFileContent.Split(" ")[0]
-$checksumFileContent = (New-Object System.Net.WebClient).DownloadString("https://download.gocd.io/binaries/$fullVersion/win/go-$type-$fullVersion-jre-64bit-setup.exe.sha256sum")
-$checksum64 = $checksumFileContent.Split(" ")[0]
+$checksum32 = (Invoke-RestMethod "https://download.gocd.io/binaries/$fullVersion/win/go-$type-$fullVersion-jre-32bit-setup.exe.sha256sum").Split(' ')[0]
+$checksum64 = (Invoke-RestMethod "https://download.gocd.io/binaries/$fullVersion/win/go-$type-$fullVersion-jre-64bit-setup.exe.sha256sum").Split(' ')[0]
+
 (Get-Content tools\chocolateyInstall.ps1.template) -replace '{{fullVersion}}', $fullVersion -replace '{{checksum-32bit}}', $checksum32 -replace '{{checksum-64bit}}', $checksum64 | out-file tools\chocolateyInstall.ps1;
 choco pack --version=$version
 
